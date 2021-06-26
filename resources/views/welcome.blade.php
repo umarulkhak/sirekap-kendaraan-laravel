6e540bb0b6f185e34002f2dc79615dcd13449038
umarulkhak <bintang.24khak@gmail.com>
2021-06-26 12:08:12 +0700

    welcome6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-
+                        <h2>SI Rekam Data Kendaraan</h2>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
